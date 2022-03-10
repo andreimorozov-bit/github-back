bdc68c06ba329be1005ae186b0efc5c67457aaf9
andrei <hz@lol.cz>
2022-03-10 12:24:48 +0300

    remove unused endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 // import { AxiosResponse } from 'axios';
 import { AppService } from './app.service';
 
 @Controller('lol')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  async getHello(): Promise<any> {
-    const response = await this.appService.getHello();
-    return response.data;
-  }
-
-  // @Get('reset')
-  // getReset() {
-  //   this.appService.getReset();
-  //   return;
-  // }
 }
